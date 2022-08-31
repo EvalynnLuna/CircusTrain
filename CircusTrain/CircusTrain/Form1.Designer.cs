0f66cbbf2c0b628f200f0961f8e20f692960a69a
EvalynnLuna <98054728+EvalynnLuna@users.noreply.github.com>
2022-08-31 15:47:54 +0200

    Design done

--- CircusTrain/CircusTrain/Form1.Designer.cs
+++ CircusTrain/CircusTrain/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace CircusTrain
 {
-    partial class Form1
+    partial class CircusTrain
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,177 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.MeercatLBL = new System.Windows.Forms.Label();
+            this.BunnyLBL = new System.Windows.Forms.Label();
+            this.WolfLBL = new System.Windows.Forms.Label();
+            this.AlpacaLBL = new System.Windows.Forms.Label();
+            this.PolarLBL = new System.Windows.Forms.Label();
+            this.ElephantLBL = new System.Windows.Forms.Label();
+            this.MeercatTB = new System.Windows.Forms.TextBox();
+            this.WolfTB = new System.Windows.Forms.TextBox();
+            this.PolarTB = new System.Windows.Forms.TextBox();
+            this.BunnyTB = new System.Windows.Forms.TextBox();
+            this.AlpacaTB = new System.Windows.Forms.TextBox();
+            this.ElephantTB = new System.Windows.Forms.TextBox();
+            this.CalculateBTN = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(546, 19);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(242, 407);
+            this.listBox1.TabIndex = 0;
+            // 
+            // MeercatLBL
+            // 
+            this.MeercatLBL.AutoSize = true;
+            this.MeercatLBL.Location = new System.Drawing.Point(183, 156);
+            this.MeercatLBL.Name = "MeercatLBL";
+            this.MeercatLBL.Size = new System.Drawing.Size(46, 13);
+            this.MeercatLBL.TabIndex = 1;
+            this.MeercatLBL.Text = "Meercat";
+            // 
+            // BunnyLBL
+            // 
+            this.BunnyLBL.AutoSize = true;
+            this.BunnyLBL.Location = new System.Drawing.Point(422, 156);
+            this.BunnyLBL.Name = "BunnyLBL";
+            this.BunnyLBL.Size = new System.Drawing.Size(37, 13);
+            this.BunnyLBL.TabIndex = 2;
+            this.BunnyLBL.Text = "Bunny";
+            // 
+            // WolfLBL
+            // 
+            this.WolfLBL.AutoSize = true;
+            this.WolfLBL.Location = new System.Drawing.Point(186, 206);
+            this.WolfLBL.Name = "WolfLBL";
+            this.WolfLBL.Size = new System.Drawing.Size(29, 13);
+            this.WolfLBL.TabIndex = 3;
+            this.WolfLBL.Text = "Wolf";
+            // 
+            // AlpacaLBL
+            // 
+            this.AlpacaLBL.AutoSize = true;
+            this.AlpacaLBL.Location = new System.Drawing.Point(419, 206);
+            this.AlpacaLBL.Name = "AlpacaLBL";
+            this.AlpacaLBL.Size = new System.Drawing.Size(40, 13);
+            this.AlpacaLBL.TabIndex = 4;
+            this.AlpacaLBL.Text = "Alpaca";
+            // 
+            // PolarLBL
+            // 
+            this.PolarLBL.AutoSize = true;
+            this.PolarLBL.Location = new System.Drawing.Point(186, 249);
+            this.PolarLBL.Name = "PolarLBL";
+            this.PolarLBL.Size = new System.Drawing.Size(55, 13);
+            this.PolarLBL.TabIndex = 5;
+            this.PolarLBL.Text = "Polar bear";
+            // 
+            // ElephantLBL
+            // 
+            this.ElephantLBL.AutoSize = true;
+            this.ElephantLBL.Location = new System.Drawing.Point(422, 256);
+            this.ElephantLBL.Name = "ElephantLBL";
+            this.ElephantLBL.Size = new System.Drawing.Size(52, 13);
+            this.ElephantLBL.TabIndex = 6;
+            this.ElephantLBL.Text = "Elephant ";
+            // 
+            // MeercatTB
+            // 
+            this.MeercatTB.Location = new System.Drawing.Point(132, 153);
+            this.MeercatTB.Name = "MeercatTB";
+            this.MeercatTB.Size = new System.Drawing.Size(45, 20);
+            this.MeercatTB.TabIndex = 7;
+            // 
+            // WolfTB
+            // 
+            this.WolfTB.Location = new System.Drawing.Point(132, 203);
+            this.WolfTB.Name = "WolfTB";
+            this.WolfTB.Size = new System.Drawing.Size(45, 20);
+            this.WolfTB.TabIndex = 8;
+            // 
+            // PolarTB
+            // 
+            this.PolarTB.Location = new System.Drawing.Point(132, 246);
+            this.PolarTB.Name = "PolarTB";
+            this.PolarTB.Size = new System.Drawing.Size(45, 20);
+            this.PolarTB.TabIndex = 9;
+            // 
+            // BunnyTB
+            // 
+            this.BunnyTB.Location = new System.Drawing.Point(371, 153);
+            this.BunnyTB.Name = "BunnyTB";
+            this.BunnyTB.Size = new System.Drawing.Size(45, 20);
+            this.BunnyTB.TabIndex = 10;
+            // 
+            // AlpacaTB
+            // 
+            this.AlpacaTB.Location = new System.Drawing.Point(371, 203);
+            this.AlpacaTB.Name = "AlpacaTB";
+            this.AlpacaTB.Size = new System.Drawing.Size(45, 20);
+            this.AlpacaTB.TabIndex = 11;
+            // 
+            // ElephantTB
+            // 
+            this.ElephantTB.Location = new System.Drawing.Point(371, 249);
+            this.ElephantTB.Name = "ElephantTB";
+            this.ElephantTB.Size = new System.Drawing.Size(45, 20);
+            this.ElephantTB.TabIndex = 12;
+            // 
+            // CalculateBTN
+            // 
+            this.CalculateBTN.Location = new System.Drawing.Point(248, 311);
+            this.CalculateBTN.Name = "CalculateBTN";
+            this.CalculateBTN.Size = new System.Drawing.Size(75, 23);
+            this.CalculateBTN.TabIndex = 13;
+            this.CalculateBTN.Text = "Calculate";
+            this.CalculateBTN.UseVisualStyleBackColor = true;
+            // 
+            // CircusTrain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.CalculateBTN);
+            this.Controls.Add(this.ElephantTB);
+            this.Controls.Add(this.AlpacaTB);
+            this.Controls.Add(this.BunnyTB);
+            this.Controls.Add(this.PolarTB);
+            this.Controls.Add(this.WolfTB);
+            this.Controls.Add(this.MeercatTB);
+            this.Controls.Add(this.ElephantLBL);
+            this.Controls.Add(this.PolarLBL);
+            this.Controls.Add(this.AlpacaLBL);
+            this.Controls.Add(this.WolfLBL);
+            this.Controls.Add(this.BunnyLBL);
+            this.Controls.Add(this.MeercatLBL);
+            this.Controls.Add(this.listBox1);
+            this.Name = "CircusTrain";
+            this.Text = "Circus train";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label MeercatLBL;
+        private System.Windows.Forms.Label BunnyLBL;
+        private System.Windows.Forms.Label WolfLBL;
+        private System.Windows.Forms.Label AlpacaLBL;
+        private System.Windows.Forms.Label PolarLBL;
+        private System.Windows.Forms.Label ElephantLBL;
+        private System.Windows.Forms.TextBox MeercatTB;
+        private System.Windows.Forms.TextBox WolfTB;
+        private System.Windows.Forms.TextBox PolarTB;
+        private System.Windows.Forms.TextBox BunnyTB;
+        private System.Windows.Forms.TextBox AlpacaTB;
+        private System.Windows.Forms.TextBox ElephantTB;
+        private System.Windows.Forms.Button CalculateBTN;
     }
 }
 
--- CircusTrain/CircusTrain/Form1.cs
+++ CircusTrain/CircusTrain/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace CircusTrain
 {
-    public partial class Form1 : Form
+    public partial class CircusTrain : Form
     {
-        public Form1()
+        public CircusTrain()
         {
             InitializeComponent();
         }
